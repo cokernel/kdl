49a49f68be599585f90ab5003dfec68d35d058a6
Michael Slone <m.slone@gmail.com>
2011-05-25 09:08:09 -0400

    only include new elements

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,25 +3,7 @@ require_dependency('vendor/plugins/blacklight_ext_ead_simple/app/helpers/catalog
 
 module Eadsax
   class Did
-    include SAXMachine
-    elements :abstract, :as => :abstracts
-    elements :container, :as => :containers#, :class => Container
-    elements :container, :value => :type, :as => :container_types#, :class => Container
-    element :dao, :class => Dao
     element :dao, :value => :entityref, :as => :dao_link
-    element :daogrp
-    element :head
-    element :langmaterial
-    element :materialspec
-    element :note, :class => Note
-    element :origination, :class => Origination
-    element :physdesc
-    element :physloc
-    element :repository
-    element :unitdate
-    element :unitid
-    element :unittitle
-
   end
 end
 
