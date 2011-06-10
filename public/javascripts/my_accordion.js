066d8d86f486effb47050cb3a6f7768104256e44
Michael Slone <m.slone@gmail.com>
2011-06-10 08:23:47 -0400

    add "contact us about this item" per local request

--- public/javascripts/my_accordion.js
+++ public/javascripts/my_accordion.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
-    $('#facets h3').next("ul, div").each(function(){
-   var f_content = $(this);
-   // find all f_content's that don't have any span descendants with a class of "selected"
-   if($('span.selected', f_content).length == 0){
-        // hide it
-        var text = $('h3', f_content.parent()).text();
+  $('#facets h3').next("ul, div").each(function() {
+    var f_content = $(this);
+    if ($('span.selected', f_content).length == 0) {
+      var text = $('h3', f_content.parent()).text();
+      $('h3', f_content.parent()).click(function() {
+        $(f_content).slideToggle();
+      });
+    }
+  });
 
-        /* Eventum request 440 (Search Facet Drop Downs in Blacklight):
-           In the same manner that we adjusted the Format facet to display its contents 
-           at all times, eliminating the drop down feature, I would like to do the same 
-           for all search facets used.
-        if (text != 'Format') {
-          f_content.hide();
-        }
-        */
-        // attach the toggle behavior to the h3 tag
-        $('h3', f_content.parent()).click(function(){
-           // toggle the content
-           $(f_content).slideToggle();
-       });
-   }
-});
+  $('.contact_us h3').next("ul, div").each(function() {
+    var f_content = $(this);
+    f_content.hide();
+    $('h3', f_content.parent()).click(function() {
+      $(f_content).slideToggle();
+    });
+  });
 });
