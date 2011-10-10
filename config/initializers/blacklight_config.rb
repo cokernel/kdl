f19c4e7f46ae631433c8175f9c864087a157f25b
Michael Slone <m.slone@gmail.com>
2011-10-10 14:35:52 -0400

    add logo for Louisville Free Public Library

--- config/initializers/blacklight_config.rb
+++ config/initializers/blacklight_config.rb
@@ -303,6 +303,7 @@ Blacklight.configure(:shared) do |config|
     'generic' => '/images/repos/logo_placeholder.png',
     'Appalshop Inc.' => '/images/repos/kap_logo.png',
     'Lexington Public Library' => '/images/repos/kyl_logo.png',
+    'Louisville Free Public Library' => '/images/repos/lfpl_logo.png',
     'Morehead State University' => '/images/repos/kmm_logo.png',
     'University of Kentucky' => '/images/repos/kuk_logo.png',
     'Transylvania University' => '/images/repos/ktu_logo.png',
