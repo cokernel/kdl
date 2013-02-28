c048f28b6feed6d04b85a01ae8687f33a45fc46b
Michael Slone <m.slone@gmail.com>
2013-02-28 12:48:04 -0500

    add address

--- app/models/catalog_mailer.rb
+++ app/models/catalog_mailer.rb
@@ -1,7 +1,7 @@
 class CatalogMailer < ActionMailer::Base
   def contact_us(repo, document, patron)
     recipients repo['email']
-    bcc        'm.slone@uky.edu'
+    bcc        ['m.slone@uky.edu', 'sarah.dorpinghaus@uky.edu']
     from       "webmaster@eris.uky.edu"
     subject    "Patron request regarding KDL item #{document[:id]}"
     body       :repo => repo, :document => document, :patron => patron
