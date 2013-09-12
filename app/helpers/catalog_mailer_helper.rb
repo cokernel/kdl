fa92605a96b42d38b12e9ef370b4d60166e32a34
Michael Slone <m.slone@gmail.com>
2013-09-12 09:26:01 -0400

    strip most characters from phone field in contact us form

--- app/helpers/catalog_mailer_helper.rb
+++ app/helpers/catalog_mailer_helper.rb
@@ -3,4 +3,8 @@ module CatalogMailerHelper
     question.gsub!(/\[([^\]]+)\].*\[\/\1\]/, '')
     sanitize(question, :tags => [])
   end
+  
+  def sanitize_phone(phone)
+    phone.gsub(/[^0-9\-+*#p\(\) ]/, '')
+  end
 end
