84188d023f13d662c6f86f617cf545b23332a010
Michael Slone <m.slone@gmail.com>
2014-09-10 09:30:52 -0400

    shut it down

--- lib/kdl.rb
+++
@@ -1,5 +0,0 @@
-require 'kdl/link_renderer'
-require 'kdl/thumb_renderer'
-require 'kdl/parser'
-
-require 'active_support/core_ext/integer/inflections'
--- spec/views/pages/about.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "/pages/about" do
-  before(:each) do
-    render 'pages/about'
-  end
-
-  #Delete this example and add some real ones or delete this file
-end
