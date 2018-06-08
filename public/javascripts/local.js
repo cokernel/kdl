93e1fcb3196d52555fc3704b85445a522da5572d
Michael Slone <m.slone@gmail.com>
2018-06-08 09:50:25 -0400

    Remove some unused JavaScript.

--- public/javascripts/local.js
+++ public/javascripts/local.js
@@ -1,39 +1,3 @@
-function crossfade(current_image, next_image) {
-  if (next_image.is(':animated')) {
-    next_image.stop().fadeTo(250, 1);
-    current_image.fadeOut(250);
-  }
-  else {
-    next_image.fadeIn(250);
-    current_image.fadeOut(250);
-  }
-}
-
-var image;
-function rewrite_image() {
-  $.get('/catalog/random', function(data) {
-    image.html(data);
-  });
-}
-
-function replace_image_left() {
-  var current_image = $('.rotato > .current_image');
-  var next_image = $('.rotato > .next_image');
-  image = current_image;
-  crossfade(current_image, next_image);
-  setTimeout('rewrite_image();', 5000);
-  setTimeout('replace_image_right();', 10000);
-}
-
-function replace_image_right() {
-  var current_image = $('.rotato > .next_image');
-  var next_image = $('.rotato > .current_image');
-  image = current_image;
-  crossfade(current_image, next_image);
-  setTimeout('rewrite_image();', 5000);
-  setTimeout('replace_image_left();', 10000);
-}
-
 jQuery(document).ready(function() {
 	$("form.addFolder, form.deleteFolder").each(function() {
 		var form = $(this);
@@ -50,38 +14,10 @@ jQuery(document).ready(function() {
 	  }
 	});	
 
-  if ($("#more_formats_list").find(".format_select").size() == 0) {
-    $("#more_formats_list").hide();
-  }
-
-  $(".more_formats").click(function() {
-    $("#more_formats_list").slideToggle('fast');
-  });
-
   $(document).ajaxSend(function(e, xhr, options) {
     var token = $("meta[name='csrf-token']").attr("content");
     xhr.setRequestHeader("X-CSRF-Token", token);
   });
 
   $('a.more_facets_link').unbind('click');
-
-/*
-  if ($('a.prev_page').length > 0) {
-    $(document).keydown(function(e) {
-      if (e.which == 37) {
-        window.location = $('a.prev_page').attr('href');
-        return false;
-      }
-    });
-  }
-
-  if ($('a.next_page').length > 0) {
-    $(document).keydown(function(e) {
-      if (e.which == 39) { 
-        window.location = $('a.next_page').attr('href');
-        return false;
-      }
-    });
-  }
-  */
 });
